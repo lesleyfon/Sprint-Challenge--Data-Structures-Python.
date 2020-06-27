a6cf0e4beb560cdd634d86483a58ce3e0a8a2ade
lesley <lesleybanadzem2017@gmail.com>
2020-06-27 13:12:57 -0700

    Finished the rign buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,22 @@
 class RingBuffer:
     def __init__(self, capacity):
+        self.capacity = capacity
+        self.oldest_elem_index = 0
+        self.storage = []
         pass
 
     def append(self, item):
+        if len(self.storage) == self.capacity:
+            self.storage.pop(self.oldest_elem_index)
+            self.storage.insert(self.oldest_elem_index, item)
+            self.oldest_elem_index += 1
+            if self.oldest_elem_index == 5:
+                self.oldest_elem_index = 0
+        else:
+            self.storage.append(item)
+
         pass
 
     def get(self):
-        pass
+
+        return self.storage
